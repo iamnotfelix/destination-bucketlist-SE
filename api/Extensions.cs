e6e7fe31215de21b627b0d0756c042963de5be6d
iamnotfelix <andrei.iliescu03@gmail.com>
2023-05-15 15:20:18 +0300

    Created extensions for the new DTOs.

--- api/Extensions.cs
+++ api/Extensions.cs
@@ -1,4 +1,6 @@
+using api.Dtos.PrivateDestination;
 using api.Dtos.PublicDestinations;
+using api.Dtos.User;
 using api.Models;
 
 namespace api
@@ -14,6 +16,44 @@ namespace api
                 Title = destination.Title,
                 Image = destination.Image,
                 Description = destination.Description
+            };
+        }
+
+        public static PrivateDestinationDto AsDto(this PrivateDestination destination)
+        {
+            return new PrivateDestinationDto
+            {
+                Id = destination.Id,
+                Geolocation = destination.Geolocation,
+                Title = destination.Title,
+                Image = destination.Image,
+                Description = destination.Description,
+                StartDate = destination.StartDate,
+                EndDate = destination.EndDate,
+                UserId = destination.UserId
+            };
+        }
+        
+        public static UserDto AsDto(this User user)
+        {
+            return new UserDto
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Roles = user.Roles
+            };
+        }
+        
+        public static DetailedUserDto AsDetailedDto(this User user)
+        {
+            return new DetailedUserDto
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Roles = user.Roles,
+                BucketList = user.Destinations is not null 
+                    ? user.Destinations.Select(d => d.AsDto()).ToList() 
+                    : new List<PrivateDestinationDto>()
             };
         }
     }
