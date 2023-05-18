0a68784e5e13a2d073b6f56efbd29aac8743f89a
Alen <91715543+913-Iaguta-Alen-Mihael@users.noreply.github.com>
2023-05-19 02:03:52 +0300

    Added destination validation logic, no implementation yet

--- api/Validators/PublicDestinationValidator.cs
+++ api/Validators/PublicDestinationValidator.cs
@@ -1,6 +1,16 @@
-﻿namespace api.Validators;
+﻿using api.Dtos.PublicDestinations;
+
+namespace api.Validators;
 
 public class PublicDestinationValidator
 {
+    public string ValidatePublicDestination(AddPublicDestinationDto destination)
+    {
+        return "";
+    }
     
+    public string ValidatePublicDestinationDto(UpdatePublicDestinationDto destination)
+    {
+        return "";
+    }
 }
