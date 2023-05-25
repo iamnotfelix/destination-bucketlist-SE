d58afbcd8b9a62c874123486eeeab92a415ef9c2
913-Iancu-Gheorghe-Aurelian <110055304+Aurelian-Iancu@users.noreply.github.com>
2023-05-25 19:16:32 +0300

    Register and login menu

--- frontend/src/components/AppMenu.tsx
+++ frontend/src/components/AppMenu.tsx
@@ -2,6 +2,9 @@ import { Box, AppBar, Toolbar, IconButton, Typography, Button } from "@mui/mater
 import { Link, useLocation } from "react-router-dom";
 import HomeIcon from '@mui/icons-material/Home';
 import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import LoginIcon from '@mui/icons-material/Login';
+
 
 export const AppMenu = () => {
 	const location = useLocation();
@@ -24,6 +27,24 @@ export const AppMenu = () => {
 					<Typography variant="h6" component="div" sx={{ mr: 5 }}>
 						Destination bucket list
 					</Typography>
+                    <Button
+						variant={path.startsWith("/register") ? "outlined" : "text"}
+						to="/register"
+						component={Link}
+						color="inherit"
+						sx={{ mr: 5 }}
+						startIcon={<AppRegistrationIcon />}>
+						Register
+					</Button>
+					<Button 
+						variant={path.startsWith("/login") ? "outlined" : "text"}
+						to="/login"
+						component={Link}
+						color="inherit"
+						sx={{ mr: 5 }}
+						startIcon={<LoginIcon />}>
+						Login
+					</Button>
 					<Button
 						variant={path.startsWith("/shelter") ? "outlined" : "text"}
 						to="/shelter"
@@ -33,7 +54,6 @@ export const AppMenu = () => {
 						startIcon={<AirplanemodeActiveIcon />}>
 						All destinations
 					</Button>
-					
 
 
 				</Toolbar>
